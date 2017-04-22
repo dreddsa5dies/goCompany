020fc14e092223f324ca167cc47ac80463e40d70
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-04-23 00:20:03 +0300

    add OGRN INN and change example

--- _examples/get.go
+++ _examples/get.go
@@ -8,12 +8,31 @@ import (
 )
 
 func main() {
+	// поиск по имени
 	name := "Ласточка"
-	result, err := gocompany.GetByName(name)
+	resultByName, err := gocompany.GetByName(name)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Println(result)
+	fmt.Println(resultByName[0].INN)
+
+	// поиск по ИНН (тип string)
+	inn := "6820002944"
+	resultByInn, err := gocompany.GetByInn(inn)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(resultByInn[0].OGRN)
+
+	// поиск по ОГРН (тип string)
+	ogrn := "1066820016416"
+	resultByOgrn, err := gocompany.GetByOgrn(ogrn)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(resultByOgrn[0].NAME)
 	os.Exit(0)
 }
--- goCompany.go
+++ goCompany.go
@@ -11,7 +11,16 @@ var (
 	baseURL = "https://ru.rus.company/"
 )
 
-// SaveData структура для базовой информации
+// SaveData структура для базовой информации формата:
+// {
+// "id" : "7030",
+// "name" : "АКЦИОНЕРНОЕ ОБЩЕСТВО "ИНСТИТУТ "СТРОЙПРОЕКТ"",
+// "shortName" : "АО "ИНСТИТУТ "СТРОЙПРОЕКТ"",
+// "ogrn" : "1027810258673",
+// "ogrnDate" : "2002-11-12T00:00:00.000",
+// "inn" : "7826688390",
+// "kpp" : "781001001"
+// }
 type SaveData struct {
 	ID        int    `json:"id"`
 	OGRN      string `json:"ogrn"`
@@ -23,20 +32,9 @@ type SaveData struct {
 	URL       string `json:"url"`
 }
 
-// GetByName возвращает массив вида:
-// [{
-// "id" : "7030",
-// "name" : "АКЦИОНЕРНОЕ ОБЩЕСТВО "ИНСТИТУТ "СТРОЙПРОЕКТ"",
-// "shortName" : "АО "ИНСТИТУТ "СТРОЙПРОЕКТ"",
-// "ogrn" : "1027810258673",
-// "ogrnDate" : "2002-11-12T00:00:00.000",
-// "inn" : "7826688390",
-// "kpp" : "781001001"
-// }]
+// GetByName возвращает массив SaveData
 func GetByName(nameCompany string) ([]SaveData, error) {
-	// GET /интеграция/компании/
-	// Поиск компании по ИНН, ОГРН или по названию. С помощью этой команды можно получить идентификатор нужной компании и далее с помощью следующих команд получить детальную информацию.
-	url := "https://ru.rus.company/интеграция/компании/?наименование=" + nameCompany + "&стр=5"
+	url := baseURL + "/интеграция/компании/?наименование=" + nameCompany + "&стр=5"
 
 	// создание массива переменных для хранения ответа
 	var companyJSON []SaveData
@@ -52,6 +50,50 @@ func GetByName(nameCompany string) ([]SaveData, error) {
 	body, err := ioutil.ReadAll(resp.Body)
 
 	// декодирование []byte в интерфейс
+	err = json.Unmarshal(body, &companyJSON)
+	if err != nil {
+		return companyJSON, err
+	}
+
+	return companyJSON, nil
+}
+
+// GetByOgrn возвращает массив SaveData
+func GetByOgrn(ogrn string) ([]SaveData, error) {
+	url := baseURL + "/интеграция/компании/?огрн=" + ogrn
+
+	var companyJSON []SaveData
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return companyJSON, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &companyJSON)
+	if err != nil {
+		return companyJSON, err
+	}
+
+	return companyJSON, nil
+}
+
+// GetByInn возвращает массив SaveData
+func GetByInn(inn string) ([]SaveData, error) {
+	url := baseURL + "/интеграция/компании/?инн=" + inn
+
+	var companyJSON []SaveData
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return companyJSON, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
 	err = json.Unmarshal(body, &companyJSON)
 	if err != nil {
 		return companyJSON, err
